45a99e3913755f5e998618e298aac488f8b17080
Gregory Hablutzel <fxkrait@disroot.org>
2021-01-29 17:48:03 -0800

    initial

--- web/scripts/js/hello.js
+++ web/scripts/js/hello.js
@@ -1,4 +1,4 @@
 function insertHello() {
-    document.getElementById("demo").innerHTML = "Hello from JS!"
-    console.log("Hello to the console!")
-  }
+  document.getElementById("demo").innerHTML = "Hello from JS!";
+  console.log("Hello to the console!");
+}

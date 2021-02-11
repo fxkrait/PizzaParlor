c6d15b4190d6a043679fb0a921e8db8745c627f9
Gregory Hablutzel <fxkrait@disroot.org>
2021-02-11 13:39:20 -0800

    push what I got

---
+++ web/scripts/order.js
@@ -0,0 +1,28 @@
+/*var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
+
+*/
+
+
+/* always keep at least 1 open by preventing the current to close itself */
+$('[data-toggle="collapse"]').on('click',function(e){
+    if ( $(this).parents('.accordion').find('.collapse.show') ){
+        var idx = $(this).index('[data-toggle="collapse"]');
+        if (idx == $('.collapse.show').index('.collapse')) {
+            // prevent collapse
+            e.stopPropagation();
+        }
+    }
+});

b235c56e3c53936d7ca164752f81b7acbe0a30a0
bluedestiny88 <bluedestiny88@gmail.com>
2021-02-11 18:44:18 -0800

    Started making additions to Cart page. -PJL 02/11/21 18:44

---
+++ web/cart.js
@@ -0,0 +1,9 @@
+function showCustomerForm() {
+  document.getElementById('customerForm').style.display = "block";
+  document.getElementById('deliveryoptions').style.display = 'none';
+}
+
+function showDeliveryOptions() {
+  document.getElementById('deliveryoptions').style.display = "block";
+  document.getElementById('customerForm').style.display = 'none';
+}

9e7346be99eb1756bd43f4bd8bb5ba7199dd7012
Gregory Hablutzel <fxkrait@disroot.org>
2021-03-16 01:23:30 -0700

    add api doc

---
+++ apidoc/api_project.js
@@ -0,0 +1,16 @@
+define({
+  "name": "TCSS 460 API Documentation",
+  "version": "0.1.0",
+  "description": "TCSS 460 API Documentation",
+  "title": "TCSS 460 API Documentation",
+  "url": "https://greghab-tcss460-w21.herokuapp.com",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2021-03-16T08:22:50.682Z",
+    "url": "https://apidocjs.com",
+    "version": "0.26.0"
+  }
+});

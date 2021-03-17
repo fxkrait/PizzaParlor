8fe2295eef51b3eca224e0af4f8aa7c374049e34
bluedestiny88 <bluedestiny88@gmail.com>
2021-03-16 19:37:29 -0700

    Made changes to FavsOrder.sql to insert one single favorited order that will be the same across all users. -PJL 03/16/21 19:37

--- FavsOrder.sql
+++ FavsOrder.sql
@@ -1,6 +1,13 @@
 DROP TABLE IF EXISTS FavOrders CASCADE;
 CREATE TABLE FavOrders (FavOrderID SERIAL PRIMARY KEY,
                         MemberID INT,
+                        OrderName VARCHAR(255) NOT NULL,
                         OrderDetails VARCHAR(255) NOT NULL,
                         FOREIGN KEY(MemberID) REFERENCES Members(MemberID)
 );
+
+
+INSERT INTO 
+    FavOrders(FavOrderID, MemberID, OrderName, NameOrderDetails)
+VALUES
+    ('0', '0', 'Big Pizza', 'xlarge deep ranch normal T T T T T T T T T T T T T T T T T T T T T T T')
